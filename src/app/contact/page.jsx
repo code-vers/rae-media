a6dcbed2d985c7b5a1fb9269ac4db7b0ff916374
FyslAhmd <info.faysal.32@gmail.com>
2026-03-07 15:17:05 +0600

    feat: Add a new contact page with metadata and render the Contact component.

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import Contact from "@/component/Contact";
+
+export const metadata = {
+  title: "Contact | RAE Media Agency",
+  description: "Contact RAE Media Agency.",
+};
+
+export default function ContactPage() {
+  return (
+    <div>
+        <Contact />
+    </div>
+  );
+}
